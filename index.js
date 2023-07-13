a537445157aededa08347bec0bd686f98d6aee1f
Yash <yashbhajbhuje67@gmail.com>
2023-07-13 23:13:18 +0530

    Added style idea find out tomorrow

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const enterDataInHtml = (data) => {
 
   document.getElementById("display").innerHTML = data.map((eachnode, i) => {
     if (i == 0) {
-      return (`<div class="eachNode">${eachnode} for 0</div>`)
+      return (`<div class="eachNode">${eachnode}</div>`)
     }
     else if (data[i - 1] > data[i]) {
-      return (`<div class="eachNode" style="">${eachnode} for left</div>`)
+      return (`<div class="eachNode" style="top:${i*80}px; left:${50-i*10}%">${eachnode}</div>`)
     }
     else {
-      return (`<div class="eachNode" style="">${eachnode} for right</div>`)
+      return (`<div class="eachNode" style="top:${i*80}px; right:${50-i*10}%">${eachnode}</div>`)
     }
   }).join('')
 
