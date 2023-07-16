bf2cab16c4b1b3da04736289a1727b5c5d0f1eeb
Yash <yashbhajbhuje67@gmail.com>
2023-07-16 14:29:32 +0530

    added line feature but needed some modification...

--- index.js
+++ index.js
@@ -21,12 +21,14 @@ const enterDataInHtml = (data) => {
       return (`<div class="eachNode" style="left:${position}px"> ${eachnode} </div>`);
     }
     else if (data[index - 1] > data[index]) {
-      position -= 50;
-      return (`<div class="eachNode" style="top:${index * 50}px; left:${position}px"> ${eachnode} </div>`);
+      position -= 80;
+      return (`<div class="eachNode" style="top:${index * 60}px; left:${position}px"> ${eachnode} </div>
+      <svg height='55' width='${window.innerWidth-10}'><line x1='${position+70}' y1='40' x2='${position-10}' y2='100' stroke='black'/></svg>`);
     }
     else {
-      position += 50;
-      return (`<div class="eachNode" style="top:${index * 50}px; left:${position}px"> ${eachnode} </div>`);
+      position += 80;
+      return (`<div class="eachNode" style="top:${index * 60}px; left:${position}px"> ${eachnode} </div>
+      <svg height='60' width='${window.innerWidth-10}'><line x1='${position-40}' y1='45' x2='${position+40}' y2='105' stroke='black'/></svg>`);
     }
   }).join('')
 
