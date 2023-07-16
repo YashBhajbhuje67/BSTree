4abf5b5bcc093e3664245e69c7a16af7b505f39f
Yash <yashbhajbhuje67@gmail.com>
2023-07-16 12:47:57 +0530

    changed i to index

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ const convertRawData = (rawDataFromInput) => {
 const enterDataInHtml = (data) => {
   
   let position = window.innerWidth/2;
-  document.getElementById("display").innerHTML = data.map((eachnode, i) => {
-    if (i == 0) {
+  document.getElementById("display").innerHTML = data.map((eachnode, index) => {
+    if (index == 0) {
       return (`<div class="eachNode" style="left:${position}px"> ${eachnode} </div>`);
     }
-    else if (data[i - 1] > data[i]) {
+    else if (data[index - 1] > data[index]) {
       position -= 50;
-      return (`<div class="eachNode" style="top:${i * 50}px; left:${position}px"> ${eachnode} </div>`);
+      return (`<div class="eachNode" style="top:${index * 50}px; left:${position}px"> ${eachnode} </div>`);
     }
     else {
       position += 50;
-      return (`<div class="eachNode" style="top:${i * 50}px; left:${position}px"> ${eachnode} </div>`);
+      return (`<div class="eachNode" style="top:${index * 50}px; left:${position}px"> ${eachnode} </div>`);
     }
   }).join('')
 
