cfa7509af84004a7417459e8ad30830ea2811f5c
Yash Bhajbhuje <yashbhajbhuje@avegen-00217.lan>
2023-07-17 09:26:19 +0530

    little changes

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const convertRawData = (rawDataFromInput) => {
 }
 
 const enterDataInHtml = (data) => {
-  
-  let position = window.innerWidth/2;
+
+  let position = window.innerWidth / 2;
   document.getElementById("display").innerHTML = data.map((eachnode, index) => {
     if (index == 0) {
       return (`<div class="eachNode" style="left:${position}px"> ${eachnode} </div>`);
@@ -23,12 +23,12 @@ const enterDataInHtml = (data) => {
     else if (data[index - 1] > data[index]) {
       position -= 80;
       return (`<div class="eachNode" style="top:${index * 60}px; left:${position}px"> ${eachnode} </div>
-      <svg height='55' width='${window.innerWidth-10}'><line x1='${position+70}' y1='40' x2='${position-10}' y2='100' stroke='black'/></svg>`);
+      <svg height='55' width='${window.innerWidth - 10}'><line x1='${position + 70}' y1='40' x2='${position - 10}' y2='100' stroke='black'/></svg>`);
     }
     else {
       position += 80;
       return (`<div class="eachNode" style="top:${index * 60}px; left:${position}px"> ${eachnode} </div>
-      <svg height='57' width='${window.innerWidth-10}'><line x1='${position-30}' y1='45' x2='${position+60}' y2='110' stroke='black'/></svg>`);
+      <svg height='57' width='${window.innerWidth - 10}'><line x1='${position - 30}' y1='45' x2='${position + 60}' y2='110' stroke='black'/></svg>`);
     }
   }).join('')
 
